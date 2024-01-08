2261df6920c4c3e9fe5dbf43a5f7fc10108b74c3
Frankie-cha <oyiengofrankline49@gmail.com>
2024-01-08 00:07:48 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,12 +4,3 @@ def element_at(my_list, idx):
         return None
     else:
         return my_list[idx]
-my_list = [1, 2, 3, 4, 5]
-
-result1 = element_at(my_list, 2)
-result2 = element_at(my_list, -1) 
-result3 = element_at(my_list, 10) 
-
-print(result1)
-print(result2)
-print(result3)
