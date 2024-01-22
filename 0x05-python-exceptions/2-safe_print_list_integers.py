b6ba308c10463a478d4537baf7e197d2413c68ae
Frankie-cha <oyiengofrankline49@gmail.com>
2024-01-21 23:32:53 -0800

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed_integers = 0
+    
+    try:
+        for i in range(x):
+            value = my_list[i]
+            try:
+                print("{:d}".format(value), end=' ')
+                printed_integers += 1
+            except (ValueError, TypeError):
+                pass  
+    except IndexError:
+        pass 
+    
+    print()  
+    return printed_integers
